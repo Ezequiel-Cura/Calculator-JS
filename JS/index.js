dabfbe457017d6d4878b07e2e916df3465acab00
Ezequiel-Cura <91684822+Ezequiel-Cura@users.noreply.github.com>
2022-08-04 21:12:43 -0300

    finalizado

--- JS/index.js
+++ JS/index.js
@@ -1,70 +1,141 @@
-//
+class Calculator{
+    constructor(prevNumber,currentNumber){
+        this.prevNumberText = prevNumber
+        this.currentNumberText = currentNumber;
+        this.clear()
+    }
 
-const input = document.getElementById("input")
-// NUMBERS
-const one = document.getElementById("one")
-const two = document.getElementById("two")
-const three= document.getElementById("three")
-const four = document.getElementById("four")
-const five = document.getElementById("five")
-const six= document.getElementById("six")
-const seven= document.getElementById("seven")
-const eight= document.getElementById("eight")
-const nine= document.getElementById("nine")
-const zero= document.getElementById("zero")
-//functions
-const rest= document.getElementById("rest")
-const addition= document.getElementById("addition")
-const multiply= document.getElementById("multiply")
-const split= document.getElementById("split")
-const result= document.getElementById("result")
-const point= document.getElementById("point")
+    
+    clear(){
+        this.prevNumber="";
+        this.currentNumber="";
+        this.operation = undefined
+        
+    }
+    delete(){
+        if(currentNumber==="")return
+        this.currentNumber = this.currentNumber.toString().slice(0,-1)
+    }
+    appendNumber(number){
+        
+        if(number === "." && this.currentNumber.includes("."))return
+        this.currentNumber = this.currentNumber.toString()+number.toString()
+    }
+    chooseOperation(operation){
+        if(this.currentNumber === "") return
+        if(this.prevNumber !== ""){
+            this.compute()
+        }
+        this.operation = operation
+        this.prevNumber = this.currentNumber
+        this.currentNumber = ""
+    }
 
+    getDisplayNumber(number){
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split(".")[0])
+        const decimalDigits = stringNumber.split(".")[1]
+        let integerDisplay
+        if(isNaN(integerDigits)){
+            integerDisplay = ""
+        }else{
+            integerDisplay = integerDigits.toLocaleString("en",{
+                maximumFractionDigits:0
+            })
+        }
+        if(decimalDigits != null){
+            return `${integerDisplay}.${decimalDigits}`
+        }else{
+            return integerDisplay
+        }
+        
+    }
+    updateDisplay(){
+        this.currentNumberText.innerHTML =
+            this.getDisplayNumber(this.currentNumber)
+        if(this.operation != null){
+            this.prevNumberText.innerHTML = `${this.prevNumber} ${this.operation}`
+        }else{
+            this.prevNumberText.innerHTML = ""
 
+        }
+        
+    }
+    compute(){
+        let computation
+        const prev = parseFloat(this.prevNumber)
+        const current = parseFloat(this.currentNumber)
+        
+        if(isNaN(prev)||isNaN(current))return
+        switch(this.operation){
+            case "+":
+                computation = prev + current
+                break;
+            case "-":
+                computation= prev - current
+                break;
+            case "*":
+                computation= prev * current
+                break;
+            case "%":
+                computation= prev / current
+                break;
+            default:
+                return
+        }
 
-one.addEventListener("click",()=>{
-    input.value += 1 
+        this.currentNumber = computation
+        this.operation = ""
+        this.prevNumber = ""
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-clear]')
+
+const prevNumber = document.querySelector('[data-prevNumber]')
+const currentNumber = document.querySelector('[data-currentNumber]')
+
+const calculator = new Calculator(prevNumber,currentNumber)
+
+numberButtons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        calculator.appendNumber(button.innerHTML)
+        calculator.updateDisplay()
+    })
 })
 
-two.addEventListener("click",()=>{
-    input.value += 2
-})
-three.addEventListener("click",()=>{
-    input.value += 3
-})
-
-four.addEventListener("click",()=>{
-    input.value += 4
+operationButtons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        calculator.chooseOperation(button.innerHTML)
+        calculator.updateDisplay()
+    })
 })
 
-five.addEventListener("click",()=>{
-    input.value += 5
+equalButton.addEventListener('click',()=>{
+    calculator.compute()
+    calculator.updateDisplay()
 })
 
-six.addEventListener("click",()=>{
-    input.value += 6
+allClearButton.addEventListener('click',()=>{
+    calculator.clear()
+    calculator.updateDisplay()
 })
 
-seven.addEventListener("click",()=>{
-    input.value += 7
+deleteButton.addEventListener('click',()=>{
+    calculator.delete()
+    calculator.updateDisplay()
 })
 
-eight.addEventListener("click",()=>{
-    input.value += 8
-})
 
-nine.addEventListener("click",()=>{
-    input.value += 9
-})
+// if(this.currentNumber[this.currentNumber.length -1]==="-")return 
+// if(this.currentNumber[this.currentNumber.length -1]==="+")return 
+// if(this.currentNumber[this.currentNumber.length -1]==="%")return 
+// if(this.currentNumber[this.currentNumber.length -1]==="*")return 
+// if(this.currentNumber[this.currentNumber.length -1]===".")return 
 
-zero.addEventListener("click",()=>{
-    input.value += 0
-})
 
-// point.addEventListener("click",()=>{
-//     input.value += "."
-// })
 
-addition.addEventListener("click",()=>{
-    input.value += "+"
-})
